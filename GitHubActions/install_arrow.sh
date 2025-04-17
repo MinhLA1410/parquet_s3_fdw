df2210313781a1bf2704431c0cf560b58366db59
MinhLA <vagrant@localhost.localdomain>
2025-04-17 11:06:14 +0700

    improve CI env

---
+++ GitHubActions/install_arrow.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+VERSION="$1"
+
+INSTALL_DIR="/tmp/arrow"
+CONFIGURE_OPTIONS="-DBUILD_SHARED_LIBS=ON -DARROW_PARQUET=ON -DARROW_WITH_SNAPPY=ON -DARROW_WITH_ZSTD=ON"
+
+echo "ARROW ver \"$VERSION\", install to \"$INSTALL_DIR\" with options \"$CONFIGURE_OPTIONS\"";
+
+adr="https://github.com/apache/arrow.git"
+echo "ARROW source code: $adr"
+git clone "$adr" $INSTALL_DIR
+cd $INSTALL_DIR
+git checkout -b apache-arrow-$VERSION refs/tags/apache-arrow-$VERSION
+git submodule update --init --recursive
+cd cpp/release
+cmake .. $CONFIGURE_OPTIONS .
+
+make
+echo "----- ARROW INSTALL directory $INSTALL_DIR -----"
+sudo make install
+
+echo "/usr/local/lib64" | sudo tee /etc/ld.so.conf.d/custom-lib64.conf
+sudo ldconfig
